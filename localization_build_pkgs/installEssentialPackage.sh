f7990ba0b1902de0d2f0721f1a9af6289dc17b88
jaehoon <tgc04042@cu.ac.kr>
2022-01-07 04:35:15 +0900

    AD-28 modified localization_build_pkgs

--- localization_build_pkgs/installEssentialPackage.sh
+++ localization_build_pkgs/installEssentialPackage.sh
@@ -2,8 +2,10 @@
 cd ~/Development/SnSAutonomousRC
 
 pip install recordclass
+pip3 install recordclass
 # this one will take a while
 pip install scikit-image
+pip3 install scikit-image
 
 sudo apt-get update
 rosdep install -r --from-paths src --ignore-src --rosdistro melodic -y
